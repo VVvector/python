b798b79911dd238179322c43ac29a0d422ecee36
VVvector <1147059951@qq.com>
2022-03-06 22:27:27 +0800

    1. add tcp rx flow kprobe to tracing tcp cc

--- eBPF/tcp_cc_perf.py
+++ eBPF/tcp_cc_perf.py
@@ -15,14 +15,15 @@ bpf_text = """
 #include <net/inet_connection_sock.h>
 #include <bcc/proto.h>
 #include <linux/tcp.h>
+#include <net/tcp.h>
 
 // define output data structure in C
 struct data_t {
     u32 pid;
     u64 ts;
     char comm[TASK_COMM_LEN];
-    u32 function;
-    u32 output[9];
+    u32 func_id;
+    u32 output[11];
 };
 BPF_PERF_OUTPUT(events);
 
@@ -38,23 +39,50 @@ struct bictcp {
     u32	delayed_ack;	/* estimate the ratio of Packets/ACKs << 4 */
 };
 
-
-static inline unsigned int tcp_left_out(const struct tcp_sock *tp)
+//workaround: avoid the compile error!!!
+static inline u32 trace_tcp_packet_in_flight(struct tcp_sock *tp)
 {
-    return tp->sacked_out + tp->lost_out;
+    u32 tcp_left_out = tp->sacked_out + tp->lost_out;
+    u32 tcp_packets_in_flight = tp->packets_out - tcp_left_out + tp->retrans_out;
+
+    return tcp_packets_in_flight;
 }
 
-static inline unsigned int tcp_packets_in_flight(const struct tcp_sock *tp)
-{
-    return tp->packets_out - tcp_left_out(tp) + tp->retrans_out;
-}
-
-int kprobe__tcp_ack(struct pt_regs *ctx, struct sock *sk, const struct sk_buff *skb, int flag)
+#if 0
+int kprobe__tcp_v4_rcv(struct pt_regs *ctx, struct sk_buff *skb)
 {
     struct data_t data = {};
-    struct tcp_sock *tp = tcp_sk(sk);
+    data.pid = bpf_get_current_pid_tgid();
+    data.ts = bpf_ktime_get_ns();
+    bpf_get_current_comm(&data.comm, sizeof(data.comm));
+    data.func_id = 0;
+    data.output[0] = 0;
+    data.output[1] = 0;
+    data.output[2] = 0;
+    data.output[3] = 0;
+    data.output[4] = 0;
+    data.output[5] = 0;
+    data.output[6] = 0;
+    data.output[7] = 0;
+    data.output[8] = 0;
+    data.output[9] = 0;
+    data.output[10] = skb->len;
+
+    events.perf_submit(ctx, &data, sizeof(data));
     
+    return 0;
+}
+#endif
+
+int kprobe__tcp_v4_do_rcv(struct pt_regs *ctx, struct sock *sk, struct sk_buff *skb)
+{
+    struct data_t data = {};
+    struct tcp_skb_cb *tcb = ((struct tcp_skb_cb *)&((skb)->cb[0]));;
+    struct tcp_sock *tp = tcp_sk(sk);
+
     u32 saddr = sk->__sk_common.skc_rcv_saddr;
+    struct inet_sock *sock = (struct inet_sock *)sk;
+    u16 sport = sock->inet_sport;
     u32 daddr = sk->__sk_common.skc_daddr;
     u16 dport = sk->__sk_common.skc_dport;
 
@@ -62,105 +90,264 @@ int kprobe__tcp_ack(struct pt_regs *ctx, struct sock *sk, const struct sk_buff *
         data.pid = bpf_get_current_pid_tgid();
         data.ts = bpf_ktime_get_ns();
         bpf_get_current_comm(&data.comm, sizeof(data.comm));
-        data.function = 1;
+        data.func_id = 1;
         data.output[0] = saddr;
-        data.output[1] = daddr;
-        data.output[2] = ntohs(dport);
-        data.output[3] = tp->snd_cwnd;
-        data.output[4] = tp->snd_ssthresh;
-        data.output[5] = tp->rcv_wnd;
-        data.output[6] = flag;
-        data.output[7] = 0;
-        data.output[8] = 0;	
-    
+        data.output[1] = ntohs(sport);
+        data.output[2] = daddr;
+        data.output[3] = ntohs(dport);
+        data.output[4] = tp->snd_cwnd;
+        data.output[5] = tp->snd_ssthresh;
+        data.output[6] = tp->rcv_wnd;
+
+        data.output[7] = tcb->ack_seq;
+        data.output[8] = 0;
+        data.output[9] = 0;
+        data.output[10] = 0;
+
         events.perf_submit(ctx, &data, sizeof(data));
     }
     
     return 0;
 }
 
-u32 kprobe__tcp_slow_start(struct pt_regs *ctx, struct tcp_sock *tp)
+bool kprobe__tcp_add_backlog(struct pt_regs *ctx, struct sock *sk, struct sk_buff *skb)
+{
+    struct data_t data = {};
+    struct tcp_skb_cb *tcb = ((struct tcp_skb_cb *)&((skb)->cb[0]));;
+    struct tcp_sock *tp = tcp_sk(sk);
+
+    u32 saddr = sk->__sk_common.skc_rcv_saddr;
+    struct inet_sock *sock = (struct inet_sock *)sk;
+    u16 sport = sock->inet_sport;
+    u32 daddr = sk->__sk_common.skc_daddr;
+    u16 dport = sk->__sk_common.skc_dport;
+
+    if (ntohs(dport) == 5201) {
+        data.pid = bpf_get_current_pid_tgid();
+        data.ts = bpf_ktime_get_ns();
+        bpf_get_current_comm(&data.comm, sizeof(data.comm));
+        data.func_id = 2;
+        data.output[0] = saddr;
+        data.output[1] = ntohs(sport);
+        data.output[2] = daddr;
+        data.output[3] = ntohs(dport);
+        data.output[4] = tp->snd_cwnd;
+        data.output[5] = tp->snd_ssthresh;
+        data.output[6] = tp->rcv_wnd;
+
+        data.output[7] = tcb->ack_seq;
+        data.output[8] = 0;
+        data.output[9] = 0;
+        data.output[10] = 0;
+
+        events.perf_submit(ctx, &data, sizeof(data));
+    }
+    
+    return 1;
+}
+
+void kprobe__tcp_rcv_established(struct pt_regs *ctx, struct sock *sk, struct sk_buff *skb)
+{
+    struct data_t data = {};
+    struct tcp_skb_cb *tcb = ((struct tcp_skb_cb *)&((skb)->cb[0]));;
+    struct tcp_sock *tp = tcp_sk(sk);
+
+    u32 saddr = sk->__sk_common.skc_rcv_saddr;
+    struct inet_sock *sock = (struct inet_sock *)sk;
+    u16 sport = sock->inet_sport;
+    u32 daddr = sk->__sk_common.skc_daddr;
+    u16 dport = sk->__sk_common.skc_dport;
+
+    if (ntohs(dport) == 5201) {
+        data.pid = bpf_get_current_pid_tgid();
+        data.ts = bpf_ktime_get_ns();
+        bpf_get_current_comm(&data.comm, sizeof(data.comm));
+        data.func_id = 3;
+        data.output[0] = saddr;
+        data.output[1] = ntohs(sport);
+        data.output[2] = daddr;
+        data.output[3] = ntohs(dport);
+        data.output[4] = tp->snd_cwnd;
+        data.output[5] = tp->snd_ssthresh;
+        data.output[6] = tp->rcv_wnd;
+
+        data.output[7] = tcb->ack_seq;
+        data.output[8] = 0;
+        data.output[9] = 0;
+        data.output[10] = 0;
+
+        events.perf_submit(ctx, &data, sizeof(data));
+    }
+}
+
+int kprobe__tcp_ack(struct pt_regs *ctx, struct sock *sk, const struct sk_buff *skb, int flag)
+{
+    struct data_t data = {};
+    struct tcp_skb_cb *tcb = ((struct tcp_skb_cb *)&((skb)->cb[0]));
+    struct tcp_sock *tp = tcp_sk(sk);
+
+    u32 saddr = sk->__sk_common.skc_rcv_saddr;
+    struct inet_sock *sock = (struct inet_sock *)sk;
+    u16 sport = sock->inet_sport;
+    u32 daddr = sk->__sk_common.skc_daddr;
+    u16 dport = sk->__sk_common.skc_dport;
+
+    if (ntohs(dport) == 5201) {
+        data.pid = bpf_get_current_pid_tgid();
+        data.ts = bpf_ktime_get_ns();
+        bpf_get_current_comm(&data.comm, sizeof(data.comm));
+        data.func_id = 4;
+        data.output[0] = saddr;
+        data.output[1] = ntohs(sport);
+        data.output[2] = daddr;
+        data.output[3] = ntohs(dport);
+        data.output[4] = tp->snd_cwnd;
+        data.output[5] = tp->snd_ssthresh;
+        data.output[6] = tp->rcv_wnd;
+
+        data.output[7] = tcb->ack_seq;
+        data.output[8] = flag;
+        data.output[9] = 0;
+        data.output[10] = 0;
+
+        events.perf_submit(ctx, &data, sizeof(data));
+    }
+
+    return 0;
+}
+
+void kprobe_tcp_fastretrans_alert(struct pt_regs *ctx, struct sock *sk, const u32 prior_snd_una,
+				  int num_dupack, int *ack_flag, int *rexmit)
+{
+    struct data_t data = {};
+    struct inet_connection_sock *icsk = inet_csk(sk);
+    struct tcp_sock *tp = tcp_sk(sk);
+
+    u32 saddr = sk->__sk_common.skc_rcv_saddr;
+    struct inet_sock *sock = (struct inet_sock *)sk;
+    u16 sport = sock->inet_sport;
+    u32 daddr = sk->__sk_common.skc_daddr;
+    u16 dport = sk->__sk_common.skc_dport;
+    u8 ca_state = *(u8 *)((u64)&tp->inet_conn.icsk_retransmits - 1);
+
+    if (ntohs(dport) == 5201) {
+        data.pid = bpf_get_current_pid_tgid();
+        data.ts = bpf_ktime_get_ns();
+        bpf_get_current_comm(&data.comm, sizeof(data.comm));
+        data.func_id = 5;
+        data.output[0] = saddr;
+        data.output[1] = ntohs(sport);
+        data.output[2] = daddr;
+        data.output[3] = ntohs(dport);
+        data.output[4] = tp->snd_cwnd;
+        data.output[5] = tp->snd_ssthresh;
+        data.output[6] = tp->rcv_wnd;
+
+        data.output[7] = num_dupack;
+        data.output[8] = ca_state;
+        data.output[9] = 0;
+        data.output[10] = 0;
+
+        events.perf_submit(ctx, &data, sizeof(data));
+    }
+}
+
+u32 kprobe__tcp_slow_start(struct pt_regs *ctx, struct tcp_sock *tp, u32 acked)
 {
     struct data_t data = {};
     struct bictcp *ca = (void *)tp->inet_conn.icsk_ca_priv;
     struct sock *skp = (struct sock *)tp;
-    
-    u32 in_flight = tcp_packets_in_flight(tp);
+
+    u32 in_flight = trace_tcp_packet_in_flight(tp);
     u32 saddr = skp->__sk_common.skc_rcv_saddr;
+    struct inet_sock *sock = (struct inet_sock *)skp;
+    u16 sport = sock->inet_sport;
     u32 daddr = skp->__sk_common.skc_daddr;
     u16 dport = skp->__sk_common.skc_dport;
-    
+
     data.pid = bpf_get_current_pid_tgid();
     data.ts = bpf_ktime_get_ns();
     bpf_get_current_comm(&data.comm, sizeof(data.comm));
-    data.function = 2;
+    data.func_id = 6;
     data.output[0] = saddr;
-    data.output[1] = daddr;
-    data.output[2] = ntohs(dport);
-    data.output[3] = tp->snd_cwnd;
-    data.output[4] = tp->snd_ssthresh;
-    data.output[5] = tp->rcv_wnd;
-    data.output[6] = in_flight;
-    data.output[7] = tp->snd_cwnd_cnt;
-    data.output[8] = ca->cnt;	
-    
+    data.output[1] = ntohs(sport);
+    data.output[2] = daddr;
+    data.output[3] = ntohs(dport);
+    data.output[4] = tp->snd_cwnd;
+    data.output[5] = tp->snd_ssthresh;
+    data.output[6] = tp->rcv_wnd;
+
+    data.output[7] = in_flight;
+    data.output[8] = tp->snd_cwnd_cnt;
+    data.output[9] = acked;
+    data.output[10] = 0;
+
     events.perf_submit(ctx, &data, sizeof(data));
-    
+
     return 0;
 };
 
-void kprobe__tcp_cong_avoid_ai(struct pt_regs *ctx, struct tcp_sock *tp)
+void kprobe__tcp_cong_avoid_ai(struct pt_regs *ctx, struct tcp_sock *tp, u32 w, u32 acked)
 {
     struct data_t data = {};
     struct bictcp *ca = (void *)tp->inet_conn.icsk_ca_priv;
     struct sock *skp = (struct sock *)tp;
-    u32 in_flight = tcp_packets_in_flight(tp);
-    
+    u32 in_flight = trace_tcp_packet_in_flight(tp);
+
     u32 saddr = skp->__sk_common.skc_rcv_saddr;
+    struct inet_sock *sock = (struct inet_sock *)skp;
+    u16 sport = sock->inet_sport;
     u32 daddr = skp->__sk_common.skc_daddr;
     u16 dport = skp->__sk_common.skc_dport;
-    
+
     data.pid = bpf_get_current_pid_tgid();
     data.ts = bpf_ktime_get_ns();
     bpf_get_current_comm(&data.comm, sizeof(data.comm));
-    data.function = 3;
+    data.func_id = 7;
     data.output[0] = saddr;
-    data.output[1] = daddr;
-    data.output[2] = ntohs(dport);
-    data.output[3] = tp->snd_cwnd;
-    data.output[4] = tp->snd_ssthresh;
-    data.output[5] = tp->rcv_wnd;
-    data.output[6] = in_flight;
-    data.output[7] = tp->snd_cwnd_cnt;
-    data.output[8] = ca->cnt;	
-    
+    data.output[1] = ntohs(sport);
+    data.output[2] = daddr;
+    data.output[3] = ntohs(dport);
+    data.output[4] = tp->snd_cwnd;
+    data.output[5] = tp->snd_ssthresh;
+    data.output[6] = tp->rcv_wnd;
+
+    data.output[7] = in_flight;
+    data.output[8] = tp->snd_cwnd_cnt;
+    data.output[9] = ca->cnt;
+    data.output[10] = acked;
+
     events.perf_submit(ctx, &data, sizeof(data));
 };
 
-#if 0
 void kprobe__bictcp_state(struct pt_regs *ctx, struct sock *sk, u8 new_state)
 {
     struct data_t data = {};
     struct tcp_sock *tp = tcp_sk(sk);
-    
+
     u32 saddr = sk->__sk_common.skc_rcv_saddr;
+    struct inet_sock *sock = (struct inet_sock *)sk;
+    u16 sport = sock->inet_sport;
     u32 daddr = sk->__sk_common.skc_daddr;
     u16 dport = sk->__sk_common.skc_dport;
-    
+
     data.pid = bpf_get_current_pid_tgid();
     data.ts = bpf_ktime_get_ns();
     bpf_get_current_comm(&data.comm, sizeof(data.comm));
-    data.function = 4;
+    data.func_id = 8;
     data.output[0] = saddr;
-    data.output[1] = daddr;
-    data.output[2] = ntohs(dport);
-    data.output[3] = tp->snd_cwnd;
-    data.output[4] = tp->snd_ssthresh;
-    data.output[5] = tp->rcv_wnd;
-    data.output[6] = new_state;
-    data.output[7] = 0;
-    data.output[8] = 0;	
-    
+    data.output[1] = ntohs(sport);
+    data.output[2] = daddr;
+    data.output[3] = ntohs(dport);
+    data.output[4] = tp->snd_cwnd;
+    data.output[5] = tp->snd_ssthresh;
+    data.output[6] = tp->rcv_wnd;
+
+    data.output[7] = new_state;
+    data.output[8] = 0;
+    data.output[9] = 0;
+    data.output[10] = 0;	
+
     events.perf_submit(ctx, &data, sizeof(data));
 };
 
@@ -168,37 +355,39 @@ void kprobe__bictcp_cong_avoid(struct pt_regs *ctx, struct sock *sk, u32 ack, u3
 {
     struct data_t data = {};
     struct tcp_sock *tp = tcp_sk(sk);
-    
+
     u16 dport = sk->__sk_common.skc_dport;
     if (ntohs(dport) == 5201) {
+        struct inet_sock *sock = (struct inet_sock *)sk;
+        u16 sport = sock->inet_sport;
         u32 saddr = sk->__sk_common.skc_rcv_saddr;
         u32 daddr = sk->__sk_common.skc_daddr;
-    
+
         u8 in_slow_start = tp->snd_cwnd < tp->snd_ssthresh;
         u8 ss_cwnd_limit = tp->snd_cwnd < 2 * tp->max_packets_out;
         u8 is_cwnd_limited = *(u8 *)((u64)&tp->tlp_high_seq - 1);
         u8 ca_state = *(u8 *)((u64)&tp->inet_conn.icsk_retransmits - 1);
-    
+
         data.pid = bpf_get_current_pid_tgid();
         data.ts = bpf_ktime_get_ns();
         bpf_get_current_comm(&data.comm, sizeof(data.comm));
-        data.function = 5;
+        data.func_id = 9;
         data.output[0] = saddr;
-        data.output[1] = daddr;
-        data.output[2] = ntohs(dport);
-        
-        data.output[3] = tp->snd_cwnd;
-        data.output[4] = tp->snd_ssthresh;
-        data.output[5] = ca_state;
-        
-        data.output[6] = in_slow_start;
-        data.output[7] = ss_cwnd_limit;
-        data.output[8] = is_cwnd_limited;
-    
+        data.output[1] = ntohs(sport);
+        data.output[2] = daddr;
+        data.output[3] = ntohs(dport);
+        data.output[4] = tp->snd_cwnd;
+        data.output[5] = tp->snd_ssthresh;
+        data.output[6] = tp->rcv_wnd;
+
+        data.output[7] = ca_state;
+        data.output[8] = in_slow_start;
+        data.output[9] = ss_cwnd_limit;
+        data.output[10] = is_cwnd_limited;
+
         events.perf_submit(ctx, &data, sizeof(data));
     }
 }
-#endif
 """
 
 
@@ -250,29 +439,29 @@ def evt_poll_perf_data(cpu, data, size):
         ebpf_info.start = event.ts
     time_s = (float(event.ts - ebpf_info.start)) / 1000000000
 
-    stats_content = "{:<16} {:<16} {:<6} f{:<2} - {:<16} -> {:<16}:{:<6} - {:<8} {:<12} {:<8} {:<8} {:<8} {:<8}".format(
+    stats_content = "{:<16} {:<16} {:<6} f{:<2} - {:<16}:{:<6} -> {:<16}:{:<6} - {:<8} {:<12} {:<8} {:<12} {:<8} {:<8} {:<8}".format(
         time_s, event.comm.decode(), event.pid,
-        event.function,
-        inet_ntoa(event.output[0]), inet_ntoa(event.output[1]), event.output[2],
-        event.output[3], event.output[4], event.output[5],
-        event.output[6], event.output[7], event.output[8])
+        event.func_id,
+        inet_ntoa(event.output[0]), event.output[1], inet_ntoa(event.output[2]), event.output[3],
+        event.output[4], event.output[5], event.output[6], event.output[7],
+        event.output[8], event.output[9], event.output[10])
 
-    print(stats_content)
+    #print(stats_content)
     save_ebpf_stats("{}\n".format(stats_content))
 
 
 def ebpf_perf_buffer_show():
     global ebpf_info
     print("tcp cc trace")
-    print("{:<16} {:<16} {:<6} {:<2} - {:<16} -> {:<16}:{:<6} - {:<8} {:<12} {:<8} {:<8} {:<8} {:<8}".format(
+    print("{:<16} {:<16} {:<6} {:<2} - {:<16}:{:<6} -> {:<16}:{:<6} - {:<8} {:<12} {:<8} {:<12} {:<8} {:<8} {:<8}".format(
         "time_s", "comm", "pid",
         "func",
-        "v1", "v2", "v3",
-        "v4", "v5", "v6", "v7", "v8", "v9"))
+        "v0", "v1", "v2","v3", "v4", "v5",
+        "v6", "v7", "v8", "v9", "v10"))
 
     try:
         ebpf_info.stats_file_handle = open(ebpf_info.stats_file, "w", encoding='UTF-8')
-        ebpf_info.b["events"].open_perf_buffer(evt_poll_perf_data)
+        ebpf_info.b["events"].open_perf_buffer(evt_poll_perf_data, page_cnt=1024)
         while 1:
             ebpf_info.b.perf_buffer_poll()
     except:
