853f545e90ece20784b5f3af53dcd19c6e0c904c
VVvector <1147059951@qq.com>
2023-06-12 21:54:20 +0800

    add xml etree parser enhancement

--- xml/etree.py
+++ xml/etree.py
@@ -12,21 +12,25 @@ def main():
     root = tree.getroot()
     print("{} {}".format(type(root), root))
 
-    for child in root:
-        print("tag:{}, attr:{}, text:{}".format(child.tag, child.attrib, child.text))
-        for tg in child.iter():
-            print(tg.text)
-
     # 获取指定元素
     print("root[0][1]: tag={},text={}".format(root[0][1].tag, root[0][1].text))
 
-    # 遍历root下所有子元素的 neighbor 标签的节点
+    # 遍历root元素下的所有一级子元素
+    for child in root:
+        print("---tag:{}, attr:{}, txt:{} ---".format(child.tag, child.attrib, child.text))
+
+    # 遍历root元素及其所有子元素
+    for child in root.iter():
+        print("---tag:{}, attr:{}, txt:{} ---".format(child.tag, child.attrib, child.text))
+
+    # 遍历root下指定标签的所有子元素
     for neighbor in root.iter('neighbor'):
         print("neighbor: {}".format(neighbor.attrib))
 
-    # 查找root下所有的 country 元素, 并且获取country的属性和子元素rank的text内容。
+    # 遍历root下 country 标签的所有一级子元素。
     for country in root.findall('country'):
         name = country.get('name')
+        # 获取country下第一个 rank 标签的子元素
         rank = country.find('rank').text
         print("country name: {}, rank: {}".format(name, rank))
 
@@ -37,6 +41,7 @@ def main():
         rank.text = str(new_rank)
         # 添加或修改属性
         rank.set('updated', 'yes')
+        print("rank:{}".format(rank.text))
 
     tree.write('output.xml')
 
